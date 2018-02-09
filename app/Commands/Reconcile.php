883aa492060bef21b8150f03a25d0b2a1a6fe098
Edward Coleridge Smith <edcoleridgesmith@gmail.com>
2018-02-09 18:34:06 +0000

    Added command boilerplate

---
+++ app/Commands/Reconcile.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Edcs\Reconciler\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Reconcile extends Command
+{
+    protected function configure()
+    {
+        $this->setName('reconcile')
+            ->setDescription('Reconciles all bank accounts in configuration')
+            ->setHelp('This command logs into your online banking and downloads your statements');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('Whoa!');
+    }
+}
