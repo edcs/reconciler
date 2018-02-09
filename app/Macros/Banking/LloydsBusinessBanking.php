a185e37edd044ce1f6da464d0ae00449d7a8eb5b
Edward Coleridge Smith <edcoleridgesmith@gmail.com>
2018-02-09 19:53:29 +0000

    Added in login macro stage 1

--- app/Macros/Banking/LloydsBusinessBanking.php
+++ app/Macros/Banking/LloydsBusinessBanking.php
@@ -7,6 +7,10 @@ class LloydsBusinessBanking extends BankingMacro
     public function login()
     {
         $this->browser->visit('https://onlinebusiness.lloydsbank.co.uk/business/logon/login.jsp')
-                      ->assertSee('Welcome to Online for Business');
+                      ->assertSee('Welcome to Online for Business')
+                      ->type('#frmLogin:strCustomerLogin_userID', env('LLOYDS_USERNAME'))
+                      ->type('#frmLogin:strCustomerLogin_pwd', env('LLOYDS_PASSWORD'))
+                      ->click('#frmLogin:btnLogin2')
+                      ->assertSee('How would you like to log on?');
     }
 }
